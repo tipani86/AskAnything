4740256c5794adc91fedc70162bd164d57040099
Tianyi Pan <tianyi.pan@clobotics.com>
2023-04-21 00:05:51 +0800

    Add extra line break between info box

--- src/app.py
+++ src/app.py
@@ -285,6 +285,7 @@ with st.spinner("Loading vector database..."):
 
 with footer:
     st.markdown(FOOTER_HTML, unsafe_allow_html=True)
+    st.write("")
     st.info(f"Note: This app uses OpenAI's GPT-4 API under the hood. Due to high demand, the API is often busy, leading to long response times. The current timeout for the API request is {TIMEOUT} seconds/try, with {N_RETRIES} retries.", icon="ℹ️")
 
 # Initialize/maintain a chat log so we can keep tabs on previous Q&As
