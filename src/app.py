7a05dfba04d27f6c4d7ebe12fae88a29bea9e1e1
Tianyi Pan <tianyi.pan@clobotics.com>
2023-06-23 16:54:43 +0800

    Fix i bug

--- src/app.py
+++ src/app.py
@@ -198,7 +198,7 @@ def get_chat_message(
     with image_container:
         st.markdown(f"<img class='chat-icon' border=0 src='{src}' width=32 height=32>", unsafe_allow_html=True)
         st.write("")
-        if i > 0:
+        if i >= 0:
             copy_to_clipboard(f"copy_{i}", contents)
 
     with contents_container:
